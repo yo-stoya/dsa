8de2f45e516c513fb84f5706278ab2e6bc12a875
Yordan Stoyanov <yordan.stoyanov3@dxc.com>
2024-03-16 19:19:18 +0200

    add test for linked list and prepend func

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-//        MyLinkedList myLinkedList = new MyLinkedList(10);
-//        System.out.println(myLinkedList.removeLast().value);
-//        System.out.println(myLinkedList.removeLast());
+        MyLinkedList myLinkedList = new MyLinkedList(1);
+        // System.out.println(myLinkedList.removeLast().value); // remove test
+        // System.out.println(myLinkedList.removeLast());  // remove test returns null when list is empty
+        myLinkedList.prepend(10);     // prepend test
+
+        myLinkedList.printList();
     }
 }
