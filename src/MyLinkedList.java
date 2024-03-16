a9ecddeaf8fab1f4311bebdacad2f83ec12a682f
Yordan Stoyanov <yordan.stoyanov3@dxc.com>
2024-03-16 19:13:49 +0200

    refactor linked list example

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -3,14 +3,6 @@ public class MyLinkedList {
     private Node tail;
     private int length;
 
-    public static class Node {
-        int value;
-        Node next;
-        public Node(int value) {
-            this.value = value;
-        }
-    }
-
     public MyLinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
@@ -28,6 +20,7 @@ public class MyLinkedList {
         tail = newNode;
         length++;
     }
+
     public Node removeLast() {
         if (length == 0) {
             return null;
@@ -48,11 +41,32 @@ public class MyLinkedList {
         return temp;
     }
 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
             System.out.println(temp.value);
             temp = temp.next;
+        }
+    }
+
+    public static class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
         }
     }
 }
