8c361e85f84535614aca1a31a8365c9ac247db6a
Yordan Stoyanov <yordan.stoyanov3@dxc.com>
2024-03-17 15:38:28 +0200

    add stack push func

---
+++ src/stack/Main.java
@@ -0,0 +1,10 @@
+package stack;
+
+public class Main {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack(4);
+
+        // myStack.push(10);        // => 10,4
+        myStack.printStack();
+    }
+}
--- src/stack/MyStack.java
+++ src/stack/MyStack.java
@@ -1,11 +1,31 @@
 package stack;
 
+import linkedlist.MyLinkedList;
+
 public class MyStack {
     private Node top;
     private int height;
 
     public MyStack(int value) {
         this.top = new Node(value);
+        height++;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height != 0) {
+            newNode.next = top;
+        }
+
+        top = newNode;
         height++;
     }
 
